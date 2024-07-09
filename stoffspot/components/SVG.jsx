a5c7d054aa320c0bae946986402a0b3dd2f420b0
Rohit Patil <superfissionr45@gmail.com>
2024-07-09 16:22:45 +0530

    SVG properties fix

--- stoffspot/components/SVG.jsx
+++ stoffspot/components/SVG.jsx
@@ -1,16 +1,68 @@
-export const Heart = () => {
+export const Heart = ({ color }) => {
   return (
     <svg
-      width="19"
-      height="16"
+      width="20"
+      height="20"
       viewBox="0 0 19 16"
-      fill="none"
+      fill={color || "none"}
       xmlns="http://www.w3.org/2000/svg"
-      className="w-6 h-6 mr-4"
     >
       <path
         d="M17.9301 6.29468C17.3103 10.4799 10.9439 14.0313 9.61558 14.7258C9.46644 14.805 9.28467 14.8097 9.13087 14.7304C7.77463 14.05 1.15188 10.5172 0.81631 6.29002C0.466763 1.92301 4.65667 -1.5678 9.54567 3.3212C13.7402 -1.56314 18.6292 1.58278 17.9301 6.29468Z"
-        stroke="black"
+        stroke={color || "black"}
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+export const User = ({ color }) => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 21"
+      fill={color || "none"}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.6174 10.3403C12.1605 10.3403 14.2221 8.27867 14.2221 5.73556C14.2221 3.19246 12.1605 1.13086 9.6174 1.13086C7.07429 1.13086 5.0127 3.19246 5.0127 5.73556C5.0127 8.27867 7.07429 10.3403 9.6174 10.3403Z"
+        stroke={color || "black"}
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.5838 19.5242H3.98816C2.2078 19.5242 0.749023 18.0654 0.749023 16.285C0.749023 14.5047 2.2078 13.0459 3.98816 13.0459H15.5838C17.3642 13.0459 18.8229 14.5047 18.8229 16.285C18.8276 18.0701 17.3688 19.5242 15.5838 19.5242Z"
+        stroke={color || "black"}
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+export const Cart = ({ color }) => {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 19 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16.0193 18.5239H3.24918C1.97216 18.5239 0.9375 17.4892 0.9375 16.2122V7.36636C0.9375 6.08935 1.97216 5.05469 3.24918 5.05469H16.0193C17.2963 5.05469 18.331 6.08935 18.331 7.36636V16.2122C18.331 17.4892 17.2963 18.5239 16.0193 18.5239Z"
+        stroke={color || "black"}
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        fill={color || "none"}
+      />
+      <path
+        d="M6.56738 8.29425V4.20221C6.56738 2.50575 7.94227 1.13086 9.63874 1.13086C11.3352 1.13086 12.7101 2.50575 12.7101 4.20221V8.29425"
+        stroke={color || "black"}
         strokeMiterlimit="10"
         strokeLinecap="round"
         strokeLinejoin="round"
